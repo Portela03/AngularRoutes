e3939c1ec270720cb2bd72208d48f95a4b66f373
Portela03 <joao.portela03@gmail.com>
2023-10-06 20:16:56 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer//footer.component';
+import { LoginComponent } from './components/login//login.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
